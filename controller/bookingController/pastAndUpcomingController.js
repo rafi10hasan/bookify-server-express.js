50458605ff7a0b1bbc9802c387b7a0fde0d309fb
Rafi Hasan <82175684+rafi10hasan@users.noreply.github.com>
2025-01-11 16:23:31 +0600

    booking controller updated

--- controller/bookingController/pastAndUpcomingController.js
+++ controller/bookingController/pastAndUpcomingController.js
@@ -1,17 +1,16 @@
 const { getUserBookingHistory } = require("../../services/booking-services/getUserBookingHistory");
 
-const bookingHistoryController = async (req, res) => {
+const pastAndUpcomingController = async (req, res , next) => {
   const { id } = req.params;
   const bookingsData = await getUserBookingHistory(id)
   try {
-   
     if (bookingsData) {
         res.status(200).json(bookingsData)
     } else {
       res.status(404).json('there are no past and upcoming booking found')
     }
   } catch (err) {
-    res.json(err);
+    next(err)
   }
 };
-module.exports = { bookingHistoryController };
+module.exports = {pastAndUpcomingController};
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { bookingCheckController } = require('../controller/bookingController/bookingCheckController');
 const { getBookingsController } = require('../controller/bookingController/getBookingsController');
 const allBookingController = require('../controller/bookingController/allBookingController');
-const { bookingHistoryController } = require('../controller/bookingController/BookingHistoryController');
+const { pastAndUpcomingController } = require('../controller/bookingController/pastAndUpcomingController');
+
+
 
 
 const router = express.Router();
 
-router.get("/verify-purchase-room/:userId/:roomId", bookingCheckController);
+router.get("/verify-purchase-room/:userId/:roomId",bookingCheckController);
 router.get("/mybookings/:id",getBookingsController);
 router.get("/all",allBookingController);
-router.get("/history/:id",bookingHistoryController)
+router.get("/history/:id",pastAndUpcomingController)
 module.exports = router
